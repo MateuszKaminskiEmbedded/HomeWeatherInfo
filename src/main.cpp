4a31de75c8d2092111b13b77f65bfc8a781d8934
Mateusz Kaminski <kaminskimateusz70@gmail.com>
2023-09-03 22:24:09 +0200

    basic platform for project

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+//#include <Arduino.h>
 #include <stdio.h>
 #include "log.h"
 #include "IRcommunication.h"
@@ -9,18 +9,14 @@ LogLibrary logger(DEBUG);
 
 void setup() {
   Serial.begin(115200);
-  ir_init();
-}
+
+ }
 
 void loop() {
-  // logger.log(DEBUG, "This is a debug message");
-  // logger.log(INFO, "This is an info message");
-  // logger.log(WARNING, "This is a warning message");
-  // logger.log(ERROR, "This is an error message");
+  logger.log(DEBUG, "This is a debug message");
+  logger.log(INFO, "This is an info message");
+  logger.log(WARNING, "This is a warning message");
+  logger.log(ERROR, "This is an error message");
 
-  // Przykładowe użycie
-    ir_send_cool_frame();
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    
-  delay(100);
+  delay(1000);
 }
