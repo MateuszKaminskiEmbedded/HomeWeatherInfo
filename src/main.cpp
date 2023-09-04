5986708f8e9b2db5fcbdad11e44100f7b5445947
Mateusz Kaminski <kaminskimateusz70@gmail.com>
2023-09-04 23:17:29 +0200

    add i2c communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
 #include <stdio.h>
 #include "log.h"
+#include <Wire.h>
+
+#define I2C_TEMP_HUM_ADDRESS 0x38
 
 LogLibrary logger(DEBUG);
 
 void setup() {
   Serial.begin(115200);
+  Wire.begin();
 
  }
 
@@ -14,6 +18,36 @@ void loop() {
   logger.log(INFO, "This is an info message");
   logger.log(WARNING, "This is a warning message");
   logger.log(ERROR, "This is an error message");
+
+   byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
 
   delay(1000);
 }
